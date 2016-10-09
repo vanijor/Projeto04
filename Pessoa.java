851a3b4e9d1a400ac77f3420630a1e4e741e0fe4
Jorge Hélio da SIlva <vanijor@hotmail.com>
2016-10-09 11:24:37 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.crud;
+
+/**
+ *
+ * @author aluno
+ */
+public class Pessoa {
+
+   private String nome;
+   private String email;
+   private String telefone;
+   private String endereco;
+   private String cpf;
+   private String rg;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    
+    
+}
